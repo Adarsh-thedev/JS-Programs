d6eb2ae56e236e024ab2fd4ba8a17d6462362d36
Adarsh-thedev <adarshpittu@gmail.com>
2019-04-20 20:30:36 +0530

    Implemented hash table

--- hashTable.js
+++ hashTable.js
@@ -6,7 +6,8 @@ class HashTable {
     _hash(key) {
         let hash = 0;
         for(let i=0; i<key.length; i++) {
-            hash = (hash + key.charCodeAt(i)*1) % this.data.length;
+            hash = (hash + key.charCodeAt(i)*1) % this.data.length; //generates a unique hash fn
+            //console.log(hash);
         }
         return hash;
     }
@@ -14,7 +15,7 @@ class HashTable {
     set(key, value) {
         let address = this._hash(key);
         if(!this.data[address]) {
-            this.data[address] = [];
+            this.data[address] = []; // if no data on the adress, create an empty array
         }
         this.data[address].push([key,value]);
         return this.data;
@@ -25,4 +26,4 @@ const myHash  = new HashTable(50);
 myHash.set('grapes',400);
 myHash.set('banana',40);
 myHash.set('apple',40);
-console.log(myHash);
+//console.log(myHash);
