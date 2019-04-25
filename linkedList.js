649db86f2ad006e58ddd99de551aa03e08d1de18
Adarsh-thedev <adarshpittu@gmail.com>
2019-04-25 17:02:53 +0530

    implemented append method

--- linkedList.js
+++ linkedList.js
@@ -4,10 +4,23 @@ class LikedList {
             value : value,
             next : null
         }
-        this.tail = this.head;
+        this.tail = this.head; // initially when we have only one item head = tail
         this.length = 1;
+    }
+
+    append(value){
+        const newNode = {
+            value : value,
+            next : null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
     }
 }
 
 const myLinkedList = new LikedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(120);
 console.log(myLinkedList);
